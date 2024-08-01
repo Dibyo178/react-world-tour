27edad428a8666267f2717aa1036f8ad0c0dae0c
Dibyo178 <sourov.mu@gmail.com>
2024-08-01 17:46:53 +0600

    some work

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './country.css';
 
 const Details = ({country}) => {
@@ -5,16 +6,26 @@ const Details = ({country}) => {
     const {name,flags,population,area,cca3} = country;
     console.log(country);
 
+    const [visited,setVisited] = useState(false);
+
+    const handleClick = ()=>{
+
+         setVisited(!visited);
+    }
+
     return (
-        <div className="c_border">
-            <h3>Country Name: {name?.common}</h3>
+        // coditionaly css uses
+        <div className={`c_border ${visited ? 'visited' : 'non_visited'}`}> 
+            <h3 style={{color:visited ? 'purple':'none'}}>Country Name: {name?.common}</h3>
 
             <img src={flags?.png} alt="" />
 
             <p>Popilation: {population}</p>
             <p>Area: {area}</p>
             <p><small>Code:{cca3}</small></p>
-            <button className='btn'>Visited</button>
+            <button onClick={handleClick} className='btn'>{visited?'Visited':'Going Vist'}</button>
+
+            {/* {visited && 'I have a visted this country .'} */}
         </div>
     );
 };
